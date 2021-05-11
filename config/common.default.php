926fc3f0dd3e7ec009e207c16626f85f596912dd
goudiev <t.goudiev@yandex.ru>
2021-05-11 09:31:33 +0300

    gitignore

---
+++ config/common.default.php
@@ -0,0 +1,9 @@
+<?php
+/* Название сайта */
+define("SITE_NAME", "aMap");
+
+/* Подключение к БД */
+define("DB_HOST", "localhost");
+define("DB_NAME", "amap");
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
